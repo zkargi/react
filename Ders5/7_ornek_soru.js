93b5f59450d3f40cd92789d6947c78d162ece74d
Zeynep <zeynepkargi02@gmail.com>
2024-01-09 20:22:46 +0300

    first commit

---
+++ Ders5/7_ornek_soru.js
@@ -0,0 +1,7 @@
+/*
+İçinde ust_alma,karekok_alma,faktoriyel_alma
+fonksiyonlarının olduğu bir modül dosyayı oluşturun.
+Bu modül dosyasının tüm fonksiyonlarını kullanan bir örnek yapın.
+*/
+
+function ust_alma()
